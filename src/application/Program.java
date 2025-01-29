c4866109a08e9f8f9bac0bb4ae7fda9b70c44f16
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2025-01-29 19:43:46 -0300

    JDBC Database querys and SQL exception handling

---
+++ src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DB.getConnection();
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("SELECT * FROM department");
+
+            while(resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        } catch(SQLException sqlException) {
+            throw new DbException(sqlException.getMessage());
+        } finally {
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
+    }
+
+}
